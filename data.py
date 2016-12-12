b6e83ef3662c3c26fb9df935b5ff74ce579197f8
MERINO Ignacio <imerino@meom-dyn107.obs.ujf-grenoble.fr>
2016-12-12 18:30:41 +0100

    cleaning

--- data.py
+++ data.py
@@ -1,37 +1,39 @@
+#Ice shelf meltwater in Gt/yr from Depoorter 2013
 valuesIS= {'ar':-39, 'ne':-36, 'ais': -39, 'w': -26, 'sha':-76, 'van':-5, 'tot':-64,
 'mu':-28, 'por':-18,'ade':-13,'mer':-5,'nin':0,'coo':-3,'ren':-7, 'dry':-5,'ris':-34,
 'sul':-28,'lan':-6,'getz':-136,'cd':-78,'thw':-69,'pig':-95,'cosg':-11, 'abb':-86,'ven':-15,
 'geo':-144,'wor':-10,'lbc':-18,'fris':-50,'brl':-26,'jf':-24}
 
-valuesIS1990= {'ar':-39, 'ne':-36, 'ais': -39, 'w': -15, 'sha':-35, 'van':-5, 'tot':-62,
-'mu':-29, 'por':-14,'ade':-10,'mer':-4,'nin':0,'coo':0,'ren':-5, 'dry':-5,'ris':-28,
-'sul':-28,'lan':-5,'getz':-72,'cd':-37,'thw':-30,'pig':-46,'cosg':-12, 'abb':-58,'ven':-7,
-'geo':-114,'wor':-7,'lbc':0,'fris':-50,'brl':-26,'jf':-24}  ##Como no puedo poner runoff negativo a lBC, los -8Gt de parte negativa se la resto a FRIS
-
+#Calving fluxes in Gt/yr from Depoorter 2013
 valuesIB= {'ar':44, 'ne':30, 'ais': 50, 'w': 31, 'sha':34, 'van':9, 'tot':28,
 'mu':23, 'por':36,'ade':7,'mer':20,'nin':23,'coo':32,'ren':1, 'dry':3,'ris':147,
 'sul':3,'lan':11,'getz':56,'cd':18,'thw':62,'pig':50,'cosg':2, 'abb':4,'ven':8,
 'geo':17,'wor':2,'lbc':25,'fris':250,'brl':46,'jf':34}
 
+#Typical Grounding Line depth in meters
 valuesGL= {'ar':600, 'ne':450, 'ais': 200, 'w': 1000, 'sha':1200, 'van':1000, 'tot':1800,
 'mu':2000, 'por':600,'ade':1000,'mer':1000,'nin':1000,'coo':600,'ren':1000, 'dry':900,'ris':1000,
 'sul':1000,'lan':600,'getz':700,'cd':1000,'thw':800,'pig':900,'cosg':500, 'abb':500,'ven':400,
 'geo':1000,'wor':200,'lbc':700,'fris':1500,'brl':850,'jf':800}
 
+#Typical ice shelf front depth in meters
 valuesFront= {'ar':200, 'ne':-200, 'ais': 250, 'w': 200, 'sha':200, 'van':200, 'tot':350,
 'mu':350, 'por':300,'ade':200,'mer':250,'nin':300,'coo':250,'ren':200, 'dry':200,'ris':200,
 'sul':200,'lan':230,'getz':330,'cd':250,'thw':250,'pig':300,'cosg':280, 'abb':250,'ven':300,
 'geo':200,'wor':150,'lbc':200,'fris':200,'brl':20,'jf':200}
 
+#Upscalling values for the ice shelf meltwater in Gt/yr from Depoorter2013
 upscaling={'westIndian':-40,'eastIndian':-82,'rossSea':-12,'amundsen':-89,'bellingshausen':-25,'weddell':-13}
 
-upscaling1990={'westIndian':-40,'eastIndian':-82,'rossSea':-12,'amundsen':-87,'bellingshausen':-25,'weddell':-13}
-
+#Upscalling values for the calving flux in Gt/yr from Depoorter2013
 upscalingIB={'westIndian':49,'eastIndian':93,'rossSea':14,'amundsen':34,'bellingshausen':10,'weddell':16}
 
 
-#Iceshelves from Depoorter definition of segments, every two values is a lon,lat pair definiing a segment
-#(ex:8 values means (lon1,lat1,lon2,lat2,lon3,lat3,lon4,lat4) there are two segments between 1 and 2, and between 3 and 4)
+#Iceshelves of segments,
+#every two values is a lon,lat pair definiing a segment
+#(ex:8 values means (lon1,lat1,lon2,lat2,lon3,lat3,lon4,lat4) 
+#This means that the ice shelf is composed by 2 segments between points 1 and 2, and between 3 and 4)
+
 lbc=[-61.93,-65.61,-61.74,-69.50]
 fris=[-61.56,-74.80,-47.97,-77.90,-44.07,-78.26,-36.11,-78.30]
 brl=[-27.15,-76.01,-12.90,-71.98]
@@ -66,7 +68,7 @@ geo=[-78.77,-73.36,-78.35,-73.01,-77.40,-72.63,-75.75,-72.89,-74.31,-73.05,-72.4
     -73.58,-72.03,-74.90,-71.55,-71.90,-69.67,-69.16,-70.16,-68.55,-70.10]
 wor=[-67.68,-69.38,-67.03,-69.02]
 
-###Sectors
+###Sectors for upscalling purpose
 westIndian=[1.5,-70.0,96.07,-66.48]
 eastIndian=[96.07,-66.48,162.09,-70.34]
 rossSea=[162.09,-70.34,-142.03,-75.51]
